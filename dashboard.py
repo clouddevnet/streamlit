4185c723c229a085356195269f3b432b9c9ee01a
clouddevnet <126808165+clouddevnet@users.noreply.github.com>
2024-06-25 10:06:36 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -108,7 +108,7 @@ if option == "Daily_review":
 
     options = st.selectbox(
         'Pick the SPDR sectors',
-        ['DAILY', 'XLK', 'SMH', 'IGV', 'CIBR', 'XLC', 'XLY', 'XLP', 'XLF', 'XLV', 'XLI', 'XLE', 'XLB', 'XLU', 'XOP', 'XHB', 'XME', 'TAN', 'BLOK', 'AIQ',  ])
+        ['DAILY', 'ETF', 'XLK', 'SMH', 'IGV', 'CIBR', 'XLC', 'XLY', 'XLP', 'XLF', 'XLV', 'XLI', 'XLE', 'XLB', 'XLU', 'XOP', 'XHB', 'XME', 'TAN', 'BLOK', 'AIQ', 'PAVE', 'JETS' ])
     
     #st.write('You selected:', options)
     
@@ -119,10 +119,10 @@ if option == "Daily_review":
     
     
     SPDR = {
-        'DAILY' : ['SPY', 'QQQ', 'DIA',  'IWO', 'VTI', 'RSP', 'QQQE', 'SDS', 'XLK', 'SMH', 'IGV', 'CIBR', 'VGT', 'FFTY', 'IBIT', 
-                   'XLC', 'XLY', 'XLP', 'XLF', 'XLV', 'XLI', 'XLE', 'XLB', 'XLU', 'XOP', 'XHB', 'XME', 'XBI', 'BLOK', 'PAVE', 'JETS', 'IVV', 'IJH', 'IJR', 'GLD',
-                   'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'META', 'NVDA', 'TSLA', 'AMD', 'COST', 'AVGO', 'MU', 'TSM', 'ASML', 
-                    'XLG', 'IDEV', 'IEMG', 'AGG', 'BND', 'TLT', 'GLD', 'SLV', 'TAN', 'ARKK' ],
+        'DAILY' : ['SPY', 'QQQ', 'DIA', 'IWO', 'VTI', 'VGT', 'XLK', 'SMH', 'IGV', 'CIBR', 'FFTY', 'IBIT', 'RSP', 'QQQE', 'SDS',
+                   'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'META', 'NVDA', 'TSLA', 'NFLX', 'AMD', 'COST', 'AVGO', 'MU', 'TSM', 'ASML', 'LRCX', 'AMAT', 'KLAC', 'QCOM', 'CRWD', 'PANW', 'ANET', 'NTAP],
+        'ETF' : ['XLC', 'XLY', 'XLP', 'XLF', 'XLV', 'XLI', 'XLE', 'XLB', 'XLU', 'XOP', 'XHB', 'XME', 'XBI', 'BLOK', 'PAVE', 'JETS', 'IVV', 'IJH', 'IJR', 'IAG',
+                    'XLG', 'IDEV', 'IEMG', 'AGG', 'BND', 'TLT', 'GLD', 'SLV', 'TAN', 'ARKK'],
         'XLK': ['MSFT', 'AAPL', 'AVGO', 'NVDA', 'CRM', 'AMD', 'ADBE', 'ACN', 'ORCL', 'CSCO', 'QCOM', 'INTU', 'IBM', 'AMAT', 'INTC', 'NOW', 'TXN', 'MU', 'LRCX', 'ADI'],
         'XLC': ['META', 'GOOGL', 'GOOG', 'NFLX', 'VZ', 'T', 'TMUS', 'EA', 'DIS', 'CMCSA', 'CHTR', 'TTWO', 'WBD', 'OMC', 'LYV', 'IPG', 'NWSA', 'MTCH', 'FOXA', 'PARA'],
         'XLY': ['AMZN', 'TSLA', 'HD', 'MCD', 'LOW', 'BKNG', 'TJX', 'NKE', 'SBUX', 'CMG', 'ABNB', 'ORLY', 'MAR', 'AZO', 'HLT', 'F', 'GM', 'DHI', 'ROST', 'LULU'],
