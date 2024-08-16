829cbc3ed556b9be00c9b471a506b14a1c4b05d7
clouddevnet <126808165+clouddevnet@users.noreply.github.com>
2024-08-15 22:12:25 -0400

    Update hourly column layout

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,9 @@ st.set_page_config(
 
 option = st.sidebar.selectbox("Watchlist?", ('Daily_review', 'SPYQQ100', 'XLKSMHIGV',  'ETF'))
 
-timing = st.sidebar.selectbox("What Timeframe?", ('Hourly', 'Daily',  'Weekly', 'Multi'))
+#timing = st.sidebar.selectbox("What Timeframe?", ('Hourly', 'Daily',  'Weekly', 'Multi'))
+
+timing = st.radio( "Pick Timeframe", ('Hourly', 'Daily',  'Weekly', 'Multi'))
 
 st.header("STOCK MARKET DASHBOARD")
 
@@ -158,8 +160,17 @@ if option == "Daily_review":
             spdr_sector = sector
             if spdr_sector in options:
 
+                #if timing == "Hourly":
+                    #for stock in symbols:
+                        #st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=h2&s=linear&ct=candle_stick&tm=d&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=10&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=21&o[2][oc]=00FF00")
+
                 if timing == "Hourly":
+                    col1, col2 = st.columns(2)
                     for stock in symbols:
+                    #st.write(stock)
+                    with col1:
+                        st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=d&s=linear&ct=candle_stick&tm=d&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=10&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=21&o[2][oc]=00FF00")
+                    with col2:
                         st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=h2&s=linear&ct=candle_stick&tm=d&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=10&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=21&o[2][oc]=00FF00")
 
                 
