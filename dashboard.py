0b10b5e88dea9171e27c426a8e2757d646cb0a88
clouddevnet <126808165+clouddevnet@users.noreply.github.com>
2024-12-04 22:35:50 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -178,15 +178,13 @@ if option == "Daily_review":
                         #st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=h2&s=linear&ct=candle_stick&tm=d&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=10&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=21&o[2][oc]=00FF00")
 
                 if timing == "Hourly":
-                    col1, col2, col3 = st.columns(3)
+                    col1, col2 = st.columns(2)
                     for stock in symbols:
                     #st.write(stock)
                         with col1:
                             st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=d&s=linear&ct=candle_stick&tm=d&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=10&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=21&o[2][oc]=00FF00")
                         with col2:
                             st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=h2&s=linear&ct=candle_stick&tm=d&o[0][ot]=ema&o[0][op]=65&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=15&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=30&o[2][oc]=00FF00")
-                        with col3:
-                            st.image(f"https://charts-node.finviz.com/chart.ashx?cs=l&t={stock}&tf=h&s=linear&ct=candle_stick&tm=d&o[0][ot]=ema&o[0][op]=65&o[0][oc]=FF0000&o[1][ot]=ema&o[1][op]=15&o[1][oc]=0077B6&o[2][ot]=ema&o[2][op]=30&o[2][oc]=00FF00")
 
                 
                 if timing == "Daily":
